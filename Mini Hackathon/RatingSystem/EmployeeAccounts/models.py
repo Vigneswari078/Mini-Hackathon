843fee15a211970f416db0e5a958cf77420bcb6b
Vigneswari078 <ganesanvigneswari@gmail.com>
2021-01-26 17:41:26 +0530

    Database connection

--- Mini Hackathon/RatingSystem/EmployeeAccounts/models.py
+++ Mini Hackathon/RatingSystem/EmployeeAccounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+#from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+class EmployeeAccounts(models.Model):
+    Name = models.CharField(null=False, blank=False,max_length=30)
+    Email = models.EmailField(max_length = 100)
+    #EmployeeID = models.IntegerField()
+    password = models.CharField(max_length=50)
+
+    def save(self,*args,**kwargs):
+        super().save(*args,**kwargs)
+    def __str__(self):
+        return self.Name
